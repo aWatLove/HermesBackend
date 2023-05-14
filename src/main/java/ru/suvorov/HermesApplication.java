f5b77ad14900e657a39a6f59fa5b95cafeb199b1
awatlove <watislove4u@gmail.com>
2023-05-15 01:06:10 +0400

    fix commit

--- src/main/java/ru/suvorov/HermesApplication.java
+++ src/main/java/ru/suvorov/HermesApplication.java
@@ -1,7 +1,12 @@
 package ru.suvorov;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HermesApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(HermesApplication.class);
     }
 }
